acdda7ca53dc455dce37be2dc10d0cfb32bfcbf2
Sarah Bland <scb4ga@virginia.edu>
2017-04-20 10:48:44 -0400

    Change base flow & quick flow
    Enable the analyses to run independently (i.e. use correct variable names). Update comments & change variable names.

--- sep.m
+++ sep.m
@@ -1,4 +1,5 @@
 %Student Name: Sarah Bland 
+%Author Name: Professor Remus Lupin  
 %Program Name: sep.m
 %Description: Separates an array of daily discharge values into its baseflow
 %and quickflow componenents using a constant-slope method. 
@@ -7,26 +8,21 @@
 
 
 function [baseflow, quickflow] = sep(Q, area)
-
 %INPUT
 % Q: daily discharge values, cfs
 % area: catchment size, square miles 
-
 %OUTPUT
 % baseflow: baseflow discharge, cfs
 % quickflow: quickflow discharge, cfs 
-
 
 %Establish output arrays 
 baseflow = zeros(1,length(Q));
 quickflow = zeros(1,length(Q));
 
-
-%Constant-slope separation line, converting cfs hr^-1 to cfs day^-1 for
-%daily data
-thresh_slope = 0.05 / area * 24; 
-
-
+%Constant-slope separation line, converting cfs hr^-1 to cfs day^-1 for daily data
+thresh_slope = 0.05.*area.*24;
+%Values need to be converted to cfs/day
+%Multiply (0.05cfs/ mi^2)(1/ hr) * (mi^2) * (24hr/ day)
 
 %Conduct body of separation 
 for t = 1:length(Q) 
@@ -47,14 +43,14 @@ for t = 1:length(Q)
 end
 
 %Make graph for visualization 
-figure(5);
-clf(5);
+figure(10);
+clf(10);
 
 time = 1:length(Q);
 plot(time,Q,'b');  
 hold on
 plot(time,baseflow,'r');
 
-xlabel('Time, days');
-ylabel('Discharge, cfs')
+xlabel('Time (days)');
+ylabel('Discharge (cfs)')
 title('Discharge Hydrograph');
